04c67b21414a9ccfe1edaf21e6e99697f841d70b
jani474747 <ms9347474786@gmail.com>
2022-03-02 22:08:55 +0530

    Day1 done

---
+++ Day1/main.js
@@ -0,0 +1,14 @@
+var a = 45;
+var b = 64;
+
+const add = require("../Day1/add");
+
+const subtract = require("../Day1/subtract");
+const multiply = require("../Day1/multiply");
+const divide = require("../Day1/divide");
+
+
+console.log(add(a,b))
+console.log(divide(a,b))
+console.log(multiply(a,b))
+console.log(subtract(a,b))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Calculator Day-1
  Day1
+
+My first Day of Unit-4. Im verymuch excited to learn a lot more things in masai.Today I realise that. I made a Right Choice to Choose masai School for my career. To Grow as Web developer.
